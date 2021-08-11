2e791ca3c77ab9eab914e90234ab353e46e2d5d0
Piotr <piotrandrzejewski@onet.eu>
2021-08-11 15:09:10 +0200

    fix(db-schema): Add constraints name

--- schemas/db-schema.sql
+++ schemas/db-schema.sql
@@ -17,8 +17,8 @@ CREATE TABLE tasks
 task_id INT(6) NOT NULL AUTO_INCREMENT,
 user_id INT(6) NOT NULL,
 task_text VARCHAR(80) NOT NULL,
-task_date DATE,
-task_completion_date DATE NULL DEFAULT NULL,
-PRIMARY KEY (task_id),
-FOREIGN KEY (user_id) REFERENCES users(user_id)
+task_date DATETIME,
+task_completion_date DATETIME NULL DEFAULT NULL,
+CONSTRAINT primary_id PRIMARY KEY (task_id),
+CONSTRAINT foreign_id FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
